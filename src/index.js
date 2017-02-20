6286a79d2e25ac8867d32b865e11bd6797b2d6fc
wjelen26 <w.jelen20@gmail.com>
2017-02-20 20:33:16 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarkdownInput from './components/MarkdownInput';
+
+ReactDOM.render(
+  <MarkdownInput />,
+  document.getElementById('root')
+);
+// TODO: sass styling, code block higlight
